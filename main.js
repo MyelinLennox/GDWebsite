ead3a0961a217481523e2cca60ebf91b06071240
Tomo Lennox <tomo@tomoLennox.com>
2024-11-07 18:45:30 -0600

    Update main.js

--- main.js
+++ main.js
@@ -23,3 +23,15 @@ function animate() {
 	renderer.render( scene, camera );
 
 }
+
+  $('.count-numbers').each(function () {
+      $(this).prop('Counter', 0).animate({
+          Counter: $(this).text()
+      }, {
+          duration: 5000,
+          easing: 'swing',
+          step: function (now) {
+              $(this).text(Math.ceil(now));
+          }
+      });
+  });
